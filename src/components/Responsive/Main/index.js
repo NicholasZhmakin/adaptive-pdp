4dc207a69fec85095537a182ef38d4ad03179224
Nicholas <nicholas@lanars.com>
2021-05-18 17:10:02 +0300

    Add partners block

--- src/components/Responsive/Main/index.js
+++ src/components/Responsive/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PersonCard from "../../UI/PersonCard";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PeopleIcon from '@material-ui/icons/People';
 
 
 const Main = () => {
@@ -15,19 +17,34 @@ const Main = () => {
             </div>
 
             <div className='network'>
+                <div className='partners'>
+                    <div className='partners-header'>
+                        <h3 className='partners-title'>Our partners</h3>
+                        <FavoriteIcon className='partners-icon' />
+                    </div>
+                    <div className='partners-list'>
+                        <img className='partners-list__img' src='./partners/partner-1.png' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-2.jpeg' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-3.jpeg' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-4.png' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-5.png' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-6.jpg' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-7.jpeg' alt='partner'/>
+                        <img className='partners-list__img' src='./partners/partner-8.jpg' alt='partner'/>
+                    </div>
+                </div>
+
                 <div className='team'>
-                    <h3>Our team</h3>
+                    <div className='team-header'>
+                        <h3 className='team-title'>Our team</h3>
+                        <PeopleIcon className='team-icon' />
+                    </div>
                     <div className='team-list'>
                         <PersonCard name={'Lily'} position={'CEO'} />
                         <PersonCard name={'Anabelle'} position={'Manager'} />
                         <PersonCard name={'Sam'} position={'Worker'} />
                         <PersonCard name={'Jack'} position={'Worker'} />
                     </div>
-
-                </div>
-
-                <div className='partners'>
-                    Partners
                 </div>
             </div>
 
