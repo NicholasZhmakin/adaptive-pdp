cbe56859b5aa503e058c5d76ad2e49f4d8cbbb37
Nicholas <nicholas@lanars.com>
2021-10-12 17:42:01 +0300

    Add restriction for container resize

--- src/components/MoveableComponent/index.js
+++ src/components/MoveableComponent/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import classnames from 'classnames';
+import cloneDeep from 'lodash/cloneDeep';
 import Moveable from "react-moveable";
 import { Frame } from "scenejs";
 import { ClickAwayListener } from '@material-ui/core';
@@ -10,12 +11,16 @@ import './MoveableComponent.scss';
 
 const MoveableComponent = ({
   bannerItem,
+  containerBanner,
+  containerFrameRef,
+  containerRef,
   selectedBannerItem,
   setSelectedBannerItem,
   setIsDraggableForContainer,
   changeBannerItemText,
   changeBannerItemStylesField,
   replaceBannerItemStyles,
+  setDirectStylesForContainerBanner,
 }) => {
 
     const frameRef = useRef(null);
@@ -28,7 +33,7 @@ const MoveableComponent = ({
 
     useEffect(() => {
       frameRef.current = new Frame({
-          ...bannerItem.styles,
+        ...bannerItem.styles,
       });
 
       moveableItemRef.current.style.cssText = frameRef.current.toCSS();
@@ -71,14 +76,84 @@ const MoveableComponent = ({
       target.style.cssText = frameRef.current.toCSS();
     }
 
-    const onDrag = ({target, top, left}) => {
+    const onDrag = ({target, top, left, height, width}) => {
       if (selectedBannerItem?.id === bannerItem.id) {
         frameRef.current.set("left", `${left}px`);
         frameRef.current.set("top", `${top}px`);
 
+        if (bannerItem.hasOwnProperty('containerId')) {
+          autoResizeContainerBannerItem(top, left, height, width);
+        }
+
         setTransform(target);
       }
     };
+
+
+    const autoResizeContainerBannerItem = (top, left, height, width) => {
+      const {
+        top: containerTop,
+        left: containerLeft,
+        width: containerWidth,
+        height: containerHeight,
+      } = containerBanner.styles;
+
+      const itemLastCoordinateX = left + width;
+      const itemLastCoordinateY = top + height;
+
+      const cloneNestedBannerItems = cloneDeep(containerBanner.nestedBannerItems);
+      const filteredNestedBannerItems = cloneNestedBannerItems.filter((item) => item.id !== bannerItem.id);
+
+      const maxWidthAndLeft = filteredNestedBannerItems.reduce((max, current) => {
+        const positionX = parseInt(current.styles.left) + parseInt(current.styles.width);
+        return positionX > max ? positionX : max;
+      }, 0);
+
+      const maxHeightAndTop = filteredNestedBannerItems.reduce((max, current) => {
+        const positionY = parseInt(current.styles.top) + parseInt(current.styles.height);
+        return positionY > max ? positionY : max;
+      }, 0);
+
+
+      if (left < 0) {
+        frameRef.current.set("left", '0px');
+        containerFrameRef.current.set("left", `${parseInt(containerLeft) - Math.abs(left)}px`)
+        containerFrameRef.current.set("width", `${parseInt(containerWidth) + Math.abs(left)}px`)
+        // if (parseInt(bannerItem.styles.left) !== 0) {
+        //   containerFrameRef.current.set("width", `${maxWidthAndLeft + Math.abs(left)}px`)
+        // } else {
+        //   containerFrameRef.current.set("width", `${parseInt(containerWidth) + Math.abs(left)}px`)
+        // }
+
+      } else if (itemLastCoordinateX > parseInt(containerWidth)) {
+        containerFrameRef.current.set("width", `${parseInt(containerWidth) + (itemLastCoordinateX - parseInt(containerWidth))}px`)
+      } else {
+        // if (itemLastCoordinateX > maxWidthAndLeft) {
+        //   containerFrameRef.current.set("width", `${itemLastCoordinateX}px`)
+        // }
+      }
+
+
+      if (top < 0) {
+        frameRef.current.set("top", '0px');
+        containerFrameRef.current.set("top", `${parseInt(containerTop) - Math.abs(top)}px`)
+        containerFrameRef.current.set("height", `${parseInt(containerHeight) + Math.abs(top)}px`)
+        // if (parseInt(bannerItem.styles.left) !== 0) {
+        //   containerFrameRef.current.set("height", `${maxHeightAndTop + Math.abs(top)}px`)
+        // } else {
+        //   containerFrameRef.current.set("height", `${parseInt(containerHeight) + Math.abs(top)}px`)
+        // }
+      } else if (itemLastCoordinateY > parseInt(containerHeight)) {
+        containerFrameRef.current.set("height", `${parseInt(containerHeight) + (itemLastCoordinateY - parseInt(containerHeight))}px`)
+      } else {
+        // if (itemLastCoordinateY > maxHeightAndTop) {
+        //   containerFrameRef.current.set("height", `${itemLastCoordinateY}px`)
+        // }
+      }
+
+
+      containerRef.current.style.cssText = containerFrameRef.current.toCSS();
+    }
 
     const onRotate = ({target, beforeDelta }) => {
         const deg = parseFloat(frameRef.current.get("transform", "rotate")) + beforeDelta;
@@ -88,10 +163,40 @@ const MoveableComponent = ({
     };
 
     const onResize = ({target, width, height, drag}) => {
-      frameRef.current.set("width", `${width}px`);
-      frameRef.current.set("height", `${height}px`);
-      frameRef.current.set("top", `${drag.top}px`);
-      frameRef.current.set("left", `${drag.left}px`);
+
+      if (bannerItem.type === 'container') {
+
+        const maxWidthAndLeft = bannerItem.nestedBannerItems.reduce((max, current) => {
+          const positionX = parseInt(current.styles.left) + parseInt(current.styles.width);
+          return positionX > max ? positionX : max;
+        }, 0);
+
+
+        const maxHeightAndTop = bannerItem.nestedBannerItems.reduce((max, current) => {
+          const positionY = parseInt(current.styles.top) + parseInt(current.styles.height);
+          return positionY > max ? positionY : max;
+        }, 0);
+
+
+        if (width > maxWidthAndLeft) {
+          frameRef.current.set("width", `${width}px`);
+        }
+
+        if (height > maxHeightAndTop) {
+          frameRef.current.set("height", `${height}px`);
+        }
+
+        frameRef.current.set("top", `${drag.top}px`);
+        frameRef.current.set("left", `${drag.left}px`);
+
+      } else {
+        frameRef.current.set("width", `${width}px`);
+        frameRef.current.set("height", `${height}px`);
+
+      }
+
+
+
 
       setTransform(target);
     };
@@ -99,6 +204,9 @@ const MoveableComponent = ({
    const handleEndAction = ({target}) => {
      if (selectedBannerItem.id === bannerItem.id) {
        replaceBannerItemStyles(bannerItem.id, target.style.cssText, bannerItem.containerId);
+       if (containerBanner) {
+         setDirectStylesForContainerBanner(containerBanner.id, containerFrameRef?.current.properties);
+       }
      }
    }
 
@@ -139,12 +247,16 @@ const MoveableComponent = ({
            <MoveableComponent
              key={nestedBannerItem.id}
              bannerItem={nestedBannerItem.id === selectedBannerItem?.id ? selectedBannerItem : nestedBannerItem}
+             containerBanner={bannerItem}
+             containerFrameRef={frameRef}
+             containerRef={moveableItemRef}
              selectedBannerItem={selectedBannerItem}
              setSelectedBannerItem={setSelectedBannerItem}
              setIsDraggableForContainer={setIsDraggableForContainer}
              changeBannerItemText={changeBannerItemText}
              changeBannerItemStylesField={changeBannerItemStylesField}
              replaceBannerItemStyles={replaceBannerItemStyles}
+             setDirectStylesForContainerBanner={setDirectStylesForContainerBanner}
            />
          );
        }));
@@ -187,6 +299,7 @@ const MoveableComponent = ({
           draggable={isNeedToDisableDraggable()}
           scalable={false}
           resizable={true}
+          renderDirections={bannerItem.type === 'container' ? ['se'] : ["n", "nw", "ne", "s", "se", "sw", "e", "w"]}
           warpable={false}
           rotatable={true}
           origin={false}
